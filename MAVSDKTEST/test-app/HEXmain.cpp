8dc07d359ef83e6328ad476856ff0551629c073b
BlessingHex <bjgallegoosagbakhore1@sheffield.ac.uk>
2022-08-06 00:57:08 +0100

    c

--- MAVSDKTEST/test-app/HEXmain.cpp
+++ MAVSDKTEST/test-app/HEXmain.cpp
@@ -2,8 +2,18 @@
 #include "../MAVSDK/install/include/mavsdk/mavsdk.h"
 
 int main(){
+  
+  mavsdk::Mavsdk* mav = {  };
+
+  for (auto system : mav->systems()) {
+		std::cout << "Found system with MAVLink system ID: " << static_cast<int>(system->get_system_id())
+			<< ", connected: " << (system->is_connected() ? "yes" : "no")
+			<< ", has autopilot: " << (system->has_autopilot() ? "yes" : "no") << '\n';
+	}
 
   std::cout << "Hello World!" << std::endl;
+
+  delete mav;
 
   return 0;
 }
